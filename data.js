9879bd3ded3c8b78a885993fac989126ff51c2e1
unknown <arvinmanda123@gmail.com>
2023-10-22 19:55:50 +0700

    Bab 3 State and Props

---
+++ data.js
@@ -0,0 +1,39 @@
+export const CategoryList = [
+    {
+      id: 1,
+      categoryName: 'Makanan',
+    },
+    {
+      id: 2,
+      categoryName: 'Minuman',
+    },
+    {
+      id: 3,
+      categoryName: 'Makanan',
+    }
+  ];
+  
+  export const BlogList = [
+    {
+      id: 1,
+      title: 'Nasi Goreng',
+      category: 'Makanan',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/6/61/Nasi_goreng_sari_laut.jpg',
+      level: 'Tingkatan Level Membuat : Sulit',
+    },
+    {
+      id: 2,
+      title: 'Jus Avoacado',
+      category: 'Minuman',
+      image: 'https://images.genpi.co/resize/1200x675-100/uploads/arsip/normal/2022/02/10/ilustrasi-jus-alpukat-foto-envato-elements-wl42.webp',
+      level: 'Tingkatan Level Membuat : Mudah',
+    },
+    {
+      id: 3,
+      title: 'Sate Madura',
+      category: 'Makanan',
+      image: 'https://asset.kompas.com/crops/qPCGH68DyIHjt2BeurBwiAK_6yc=/103x66:966x642/750x500/data/photo/2021/03/27/605ed24c33816.jpg',
+      level: 'Tingkatan Level Membuat : Sedang',
+    },
+  ];
+  
